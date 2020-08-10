412e1c841a88915a9e706edada1c2513e7aeb86b
Regivaldo Silva <regivaldo@ciandt.com>
2020-08-10 13:49:43 -0300

    Fix a disabled button on form

--- projects/ugla/src/lib/components/form/form.component.ts
+++ projects/ugla/src/lib/components/form/form.component.ts
@@ -5,62 +5,6 @@ import { NgClass } from '@angular/common';
 
 /**
  * Form
- *
- * @example
- * <ugl-form
- *   [hiddenButtonSubmit]="false|true"
- *   (cancelClick)="Cancel EventEmitter"
- *   (submitClick)="Submit EventEmitter"></ugl-form>
- *
- * // Disable submit button
- * @ViewChild(FormComponent) formComponent: FormComponent;
- * (...)
- * this.formComponent.disableSubmitButton = true;
- *
- * // HTML
- * <section id="form" class="central">
- *    <ugl-page-title
- *       [firstTitle]="'New'"
- *       [secondTitle]="'Form Example'">
- *    </ugl-page-title>
- *    <ugl-form
- *       [hiddenButtonSubmit]="false"
- *       (cancelClick)="onCancelClick()"
- *       (submitClick)="onSubmitClick()"
- *       [cancelText]="'Cancelar'"
- *       [submitText]="'Enviar'">
- *
- *    <div uglGrid [grid]="true" [col]="'2'" [colSm]="'1'" [gap]="'2'" [gapSm]="'1'" class="form-areas">
- *       <div class="form-area-left">
- *         <ugl-select
- *           [label]="'Language'"
- *           [select]="select"
- *           [message]="'Select your language'"
- *           (selected)="selectLanguage($event)">
- *           </ugl-select>
- *
- *          <ugl-datepicker
- *            [name]="'dateTest'"
- *            [label]="'Date'"
- *            [message]="'Selecione uma Data'"
- *            [required]="true"
- *            [disabled]="false"
- *            [options]="this.onInitDatepicker()"></ugl-datepicker>
- *
- *          <ugl-field
- *            [type]="'time'"
- *            [name]="'hour'"
- *            [label]="'Hours'"
- *            [message]="'Add a value'"></ugl-field>
- *    </div>
- *
- *    <div uglGrid [grid]="true" [col]="'5'" [colSm]="'1'" class="form-area-right">
- *         <div uglGrid [span]="'1'" [spanSm]="'0'"></div>
- *         <ugl-file-upload [pondOptions]="pondOptions" uglGrid [span]="'2'" ></ugl-file-upload>
- *    </div>
- *    </div>
- *    </ugl-form>
- * </section>
  */
 @Component({
   selector: 'ugl-form',
@@ -119,7 +63,7 @@ export class FormComponent implements OnInit {
    * Disable submit button
    * Default: false
    */
-  disableSubmitButton?: boolean;
+  @Input() disableSubmitButton?: boolean;
 
   /**
    * Call cancel event.
