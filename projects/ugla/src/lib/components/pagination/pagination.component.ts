904486206e0f228f9954606e1a4b8be7e9c3176b
Regivaldo Silva <regivaldorfs@gmail.com>
2021-06-25 17:13:37 -0300

    Fix pagination tootlip

--- projects/ugla/src/lib/components/pagination/pagination.component.ts
+++ projects/ugla/src/lib/components/pagination/pagination.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit, Input, Output, EventEmitter, ViewChild, OnChanges, SimpleChanges } from '@angular/core';
+import {
+	Component,
+	OnInit,
+	Input,
+	Output,
+	EventEmitter,
+	ViewChild,
+	OnChanges,
+	SimpleChanges
+} from '@angular/core';
 import { PaginationItemsPerPageComponent } from './pagination-items-per-page/pagination-items-per-page.component';
 import { Select } from '../../models';
 import { UglaService } from '../../ugla.service';
@@ -27,418 +36,449 @@ const FIRST_PAGE = 1;
  * [previousPageButtonTitle]="'Previous Page'"
  * [nextPageButtonTitle]="'Next Page'"
  * [lastPageButtonTitle]="'Last Page'"
+ * [labelPage]="'Page'"
  * [id]="'pagination'">
  * </ugl-pagination>
  */
 @Component({
-  selector: 'ugl-pagination',
-  templateUrl: './pagination.component.html',
-  styleUrls: ['./pagination.component.scss']
+	selector: 'ugl-pagination',
+	templateUrl: './pagination.component.html',
+	styleUrls: ['./pagination.component.scss']
 })
 export class PaginationComponent implements OnInit, OnChanges {
+	/**
+	 * Internal property for limit interval
+	 */
+	private _limitInterval: number = Math.trunc(this.visiblePages / 2);
 
-  /**
-   * Internal property for limit interval
-   */
-  private _limitInterval: number = Math.trunc(this.visiblePages / 2);
+	/**
+	 * Internal property for visible pages
+	 */
+	private _visiblePages = 10;
 
-  /**
-   * Internal property for visible pages
-   */
-  private _visiblePages = 10;
+	/**
+	 * Property for first visible page
+	 */
+	private firstVisiblePage: number;
 
-  /**
-   * Property for first visible page
-   */
-  private firstVisiblePage: number;
+	/**
+	 * Property for first last page
+	 */
+	private lastVisiblePage: number;
 
-  /**
-   * Property for first last page
-   */
-  private lastVisiblePage: number;
+	/**
+	 * Property for indicating if is next page
+	 */
+	private isNext = true;
 
-  /**
-   * Property for indicating if is next page
-   */
-  private isNext = true;
+	/**
+	 * The selected page.
+	 */
+	@Input() selectedPage: number;
 
-  /**
-   * The selected page.
-   */
-  @Input() selectedPage: number;
+	/**
+	 * Total pages of the pagination.
+	 */
+	@Input() totalPages: number;
 
-  /**
-   * Total pages of the pagination.
-   */
-  @Input() totalPages: number;
+	/**
+	 * Pagination size options.
+	 */
+	@Input() itemsPerPageRange: Select;
 
-  /**
-   * Pagination size options.
-   */
-  @Input() itemsPerPageRange: Select;
+	/**
+	 * Selected pagination size.
+	 */
+	@Input() itemsPerPage: number;
 
-  /**
-   * Selected pagination size.
-   */
-  @Input() itemsPerPage: number;
+	/**
+	 * Show text before select input.
+	 */
+	@Input() itemsPerPageBeforeText? = 'Show';
 
-  /**
-   * Show text before select input.
-   */
-  @Input() itemsPerPageBeforeText?: string;
+	/**
+	 * Show text after select input.
+	 */
+	@Input() itemsPerPageAfterText? = 'Items per page';
 
-  /**
-   * Show text after select input.
-   */
-  @Input() itemsPerPageAfterText?: string;
+	/**
+	 * Component id.
+	 */
+	@Input() id: string;
 
-  /**
-   * Component id.
-   */
-  @Input() id: string;
+	/**
+	 * Set a title to the first page button.
+	 */
+	@Input() firstPageButtonTitle? = 'First Page';
 
-  /**
-   * Set a title to the first page button.
-   */
-  @Input() firstPageButtonTitle?: string;
+	/**
+	 * Set a title to the previous page button.
+	 */
+	@Input() previousPageButtonTitle? = 'Previous Page';
 
-  /**
-   * Set a title to the previous page button.
-   */
-  @Input() previousPageButtonTitle?: string;
+	/**
+	 * Set a title to the last page button.
+	 */
+	@Input() lastPageButtonTitle? = 'Last Page';
 
-  /**
-   * Set a title to the last page button.
-   */
-  @Input() lastPageButtonTitle?: string;
+	/**
+	 * Set a title to the next page button.
+	 */
+	@Input() nextPageButtonTitle? = 'Next Page';
 
-  /**
-   * Set a title to the next page button.
-   */
-  @Input() nextPageButtonTitle?: string;
+	/**
+	 * Set a title the item per page select.
+	 */
+	@Input() itemsPerPageTitle?: string;
 
-  /**
-   * Set a title the item per page select.
-   */
-  @Input() itemsPerPageTitle?: string;
+	/**
+	 * Set a label for page number.
+	 */
+	@Input() labelPage? = 'Page';
 
-  /**
-   * Set a label for page number.
-   */
-  @Input() labelPage?: string;
+	/**
+	 * Items per page component.
+	 */
+	@ViewChild('itemsPerPageComponent')
+	itemsPerPageComponent!: PaginationItemsPerPageComponent;
 
-  /**
-   * Items per page component.
-   */
-  @ViewChild('itemsPerPageComponent') itemsPerPageComponent !: PaginationItemsPerPageComponent;
+	/**
+	 * Emitter for changed pagination size.
+	 */
+	@Output()
+	changePaginationPageSize: EventEmitter<number> = new EventEmitter<number>();
 
+	/**
+	 * Emitter for changed pagination page.
+	 */
+	@Output()
+	changePaginationPage: EventEmitter<number> = new EventEmitter<number>();
 
-  /**
-   * Emitter for changed pagination size.
-   */
-  @Output() changePaginationPageSize: EventEmitter<number> = new EventEmitter<number>();
+	/**
+	 * Pages to be displayed
+	 */
+	public pages: number[];
 
-  /**
-   * Emitter for changed pagination page.
-   */
-  @Output() changePaginationPage: EventEmitter<number> = new EventEmitter<number>();
+	/**
+	 * Receives the theme's name
+	 */
+	public theme: string;
 
-  /**
-   * Pages to be displayed
-   */
-  public pages: number[];
+	/**
+	 * Receives the component's name
+	 * @param ugla: UglaService
+	 */
+	constructor(private ugla: UglaService) {
+		this.theme = ugla.theme;
+	}
 
-  /**
-   * Receives the theme's name
-   */
-  public theme: string;
+	/**
+	 * Set initials configurations
+	 */
+	ngOnInit(): void {
+		this.firePageEvent();
 
-  /**
-   * Receives the component's name
-   * @param ugla: UglaService
-   */
-  constructor(private ugla: UglaService) {
-    this.theme = ugla.theme;
-  }
+		setTimeout(() => {
+			if (this.itemsPerPageRange) {
+				this.itemsPerPageComponent.setSelected(this.itemsPerPage);
+			}
+		});
+	}
 
-  /**
-   * Set initials configurations
-   */
-  ngOnInit() {
-    this.firePageEvent();
+	ngOnChanges(changes: SimpleChanges): void {
+		if (changes.totalPages) {
+			this.totalPages = changes.totalPages.currentValue;
+		}
+		if (changes.selectedPage) {
+			this.selectedPage = changes.selectedPage.currentValue;
+		}
+		if (changes.itemsPerPage) {
+			this.itemsPerPage = changes.itemsPerPage.currentValue;
+		}
+		if (changes.itemsPerPageRange) {
+			this.itemsPerPageRange = changes.itemsPerPageRange.currentValue;
+		}
 
-    setTimeout(() => {
-      if (this.itemsPerPageRange) {
-        this.itemsPerPageComponent.setSelected(this.itemsPerPage);
-      }
-    });
-  }
+		this.firePageEvent();
+	}
 
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes.totalPages) {
-      this.totalPages = changes.totalPages.currentValue;
-    }
-    if (changes.selectedPage) {
-      this.selectedPage = changes.selectedPage.currentValue;
-    }
-    if (changes.itemsPerPage) {
-      this.itemsPerPage = changes.itemsPerPage.currentValue;
-    }
-    if (changes.itemsPerPageRange) {
-      this.itemsPerPageRange = changes.itemsPerPageRange.currentValue;
-    }
-    this.firePageEvent();
-  }
+	/**
+	 * Set pagination length
+	 */
+	@Input()
+	set visiblePages(visiblePages: number) {
+		if (visiblePages) {
+			this._visiblePages = visiblePages;
+		}
+	}
 
-  /**
-   * Set pagination length
-   */
-  @Input()
-  set visiblePages(visiblePages: number) {
-    if (visiblePages) {
-      this._visiblePages = visiblePages;
-    }
-  }
+	/**
+	 * Get pagination length
+	 */
+	get visiblePages(): number {
+		return this._visiblePages;
+	}
 
-  /**
-   * Get pagination length
-   */
-  get visiblePages() {
-    return this._visiblePages;
-  }
+	/**
+	 * Set limit interval to show pagination
+	 */
+	@Input()
+	set limitInterval(limitInterval: number) {
+		if (limitInterval) {
+			this._limitInterval = limitInterval;
+		}
+	}
 
-  /**
-   * Set limit interval to show pagination
-   */
-  @Input()
-  set limitInterval(limitInterval: number) {
-    if (limitInterval) {
-      this._limitInterval = limitInterval;
-    }
-  }
+	/**
+	 * Get limit interval to show pagination
+	 */
+	get limitInterval(): number {
+		return this._limitInterval;
+	}
 
-  /**
-   * Get limit interval to show pagination
-   */
-  get limitInterval() {
-    return this._limitInterval;
-  }
+	/**
+	 * After fire pagination, update the visible pages.
+	 */
+	changePagination(): void {
+		this.pages = [];
+		for (
+			let i = this.firstVisiblePage, len = this.lastVisiblePage;
+			i <= len;
+			i++
+		) {
+			this.pages.push(i);
+		}
+	}
 
-  /**
-   * After fire pagination, update the visible pages.
-   */
-  changePagination() {
-    this.pages = [];
-    for (let i = this.firstVisiblePage, len = this.lastVisiblePage; i <= len; i++) {
-      this.pages.push(i);
-    }
-  }
+	/**
+	 * Change navigation to previous page, and emit the selected page number.
+	 */
+	firePreviousPageEvent(): boolean {
+		if (this.selectedPage === FIRST_PAGE || !this.isPreviousPageEnabled()) {
+			return false;
+		}
 
-  /**
-   * Change navigation to previous page, and emit the selected page number.
-   */
-  firePreviousPageEvent() {
-    if (this.selectedPage === FIRST_PAGE || !this.isPreviousPageEnabled()) {
-        return false;
-    }
+		this.selectedPage = --this.selectedPage;
+		this.isNext = false;
+		this.firePageEvent();
+		this.changePaginationPage.emit(this.selectedPage);
 
-    this.selectedPage = --this.selectedPage;
-    this.isNext = false;
-    this.firePageEvent();
-    this.changePaginationPage.emit(this.selectedPage);
+		return false;
+	}
 
-    return false;
-  }
+	/**
+	 * Change navigation to first page, and emit the selected page number.
+	 */
+	fireFirstPageEvent(): boolean {
+		if (this.selectedPage === FIRST_PAGE) {
+			return false;
+		}
 
-  /**
-   * Change navigation to first page, and emit the selected page number.
-   */
-  fireFirstPageEvent() {
-    if (this.selectedPage === FIRST_PAGE) {
-        return false;
-    }
+		this.selectedPage = FIRST_PAGE;
+		this.isNext = false;
+		this.firePageEvent();
+		this.changePaginationPage.emit(this.selectedPage);
 
-    this.selectedPage = FIRST_PAGE;
-    this.isNext = false;
-    this.firePageEvent();
-    this.changePaginationPage.emit(this.selectedPage);
+		return false;
+	}
 
-    return false;
-  }
+	/**
+	 * Change navigation to next page, and emit the selected page number.
+	 */
+	fireNextPageEvent(): boolean {
+		if (
+			this.selectedPage === this.totalPages ||
+			!this.isNextPageEnabled()
+		) {
+			return false;
+		}
 
-  /**
-   * Change navigation to next page, and emit the selected page number.
-   */
-  fireNextPageEvent() {
-    if (this.selectedPage === this.totalPages || !this.isNextPageEnabled()) {
-        return false;
-    }
+		this.selectedPage = ++this.selectedPage;
+		this.isNext = true;
+		this.firePageEvent();
+		this.changePaginationPage.emit(this.selectedPage);
 
-    this.selectedPage = ++this.selectedPage;
-    this.isNext = true;
-    this.firePageEvent();
-    this.changePaginationPage.emit(this.selectedPage);
+		return false;
+	}
 
-    return false;
-  }
+	/**
+	 * Change navigation to the last page, and emit the selected page number.
+	 */
+	fireLastPageEvent(): boolean {
+		if (this.selectedPage === this.totalPages) {
+			return false;
+		}
 
-  /**
-   * Change navigation to the last page, and emit the selected page number.
-   */
-  fireLastPageEvent() {
-    if (this.selectedPage === this.totalPages) {
-        return false;
-    }
+		this.selectedPage = this.totalPages;
+		this.isNext = true;
+		this.firePageEvent();
+		this.changePaginationPage.emit(this.selectedPage);
 
-    this.selectedPage = this.totalPages;
-    this.isNext = true;
-    this.firePageEvent();
-    this.changePaginationPage.emit(this.selectedPage);
+		return false;
+	}
 
-    return false;
-  }
+	/**
+	 * Fire page navigation.
+	 *
+	 * @param page is a selected page
+	 */
+	fireNavigatePageEvent(page: number): boolean {
+		this.selectedPage = page;
+		this.changePaginationPage.emit(this.selectedPage);
+		return false;
+	}
 
-  /**
-   * Fire page navigation.
-   *
-   * @param page is a selected page
-   */
-  fireNavigatePageEvent(page) {
-    this.selectedPage = page;
-    this.changePaginationPage.emit(this.selectedPage);
-    return false;
-  }
+	/**
+	 * Return true if the previous page is enabled.
+	 */
+	isPreviousPageEnabled(): boolean {
+		return this.selectedPage > FIRST_PAGE;
+	}
 
-  /**
-   * Return true if the previous page is enabled.
-   */
-  isPreviousPageEnabled(): boolean {
-    return this.selectedPage > FIRST_PAGE;
-  }
+	/**
+	 * Return true if the next page is enabled.
+	 */
+	isNextPageEnabled(): boolean {
+		return this.selectedPage <= this.totalPages;
+	}
 
-  /**
-   * Return true if the next page is enabled.
-   */
-  isNextPageEnabled(): boolean {
-    return this.selectedPage <= this.totalPages;
-  }
+	/**
+	 * Return true if the selected pages is the current.
+	 */
+	isCurrentPage(page: number): boolean {
+		return this.selectedPage === page;
+	}
 
-  /**
-   * Return true if the selected pages is the current.
-   */
-  isCurrentPage(page): boolean {
-    return this.selectedPage === page;
-  }
+	/**
+	 * Return true if the selected pages is the first page.
+	 */
+	isFirstPage(): boolean {
+		return this.selectedPage === FIRST_PAGE;
+	}
 
-  /**
-   * Return true if the selected pages is the first page.
-   */
-  isFirstPage(): boolean {
-    return this.selectedPage === FIRST_PAGE;
-  }
+	/**
+	 * Return true if the selected pages is the last page.
+	 */
+	isLastPage(): boolean {
+		return this.selectedPage === this.totalPages;
+	}
 
-  /**
-   * Return true if the selected pages is the last page.
-   */
-  isLastPage(): boolean {
-    return this.selectedPage === this.totalPages;
-  }
+	/**
+	 * Fire update pagination.
+	 */
+	firePageEvent(): void {
+		let leftInterval: number;
+		let rightInterval: number;
 
-  /**
-   * Fire update pagination.
-   */
-  firePageEvent() {
-    let leftInterval: number;
-    let rightInterval: number;
+		if (this.isNext) {
+			rightInterval =
+				this.limitInterval === this.visiblePages
+					? this.visiblePages - 1
+					: this.limitInterval;
+			leftInterval =
+				this.limitInterval === this.visiblePages
+					? this.visiblePages - this.limitInterval
+					: this.visiblePages - 1 - this.limitInterval;
+		} else {
+			leftInterval =
+				this.limitInterval === this.visiblePages
+					? this.visiblePages - 1
+					: this.limitInterval;
+			rightInterval =
+				this.limitInterval === this.visiblePages
+					? this.visiblePages - this.limitInterval
+					: this.visiblePages - 1 - this.limitInterval;
+		}
 
-    if (this.isNext) {
-      rightInterval = (this.limitInterval === this.visiblePages)
-        ? this.visiblePages - 1 : this.limitInterval;
-      leftInterval = (this.limitInterval === this.visiblePages)
-        ? this.visiblePages - this.limitInterval : (this.visiblePages - 1) - this.limitInterval;
-    } else {
-      leftInterval =  (this.limitInterval === this.visiblePages)
-        ? this.visiblePages - 1 : this.limitInterval;
-      rightInterval = (this.limitInterval === this.visiblePages)
-        ? this.visiblePages - this.limitInterval : (this.visiblePages - 1) - this.limitInterval;
-    }
+		if (
+			(!this.firstVisiblePage && !this.lastVisiblePage) ||
+			this.isFirstPage() ||
+			this.isLastPage()
+		) {
+			this.lastVisiblePage = this.getNextLastPage(
+				this.selectedPage,
+				rightInterval
+			);
+			this.firstVisiblePage = this.getNextFirstPage(this.lastVisiblePage);
+		}
 
-    if (!this.firstVisiblePage && !this.lastVisiblePage
-            || this.isFirstPage()
-            || this.isLastPage()) {
-        this.lastVisiblePage = this.getNextLastPage(this.selectedPage, rightInterval);
-        this.firstVisiblePage = this.getNextFirstPage(this.lastVisiblePage);
-    }
+		if (
+			!this.isLeftInterval(leftInterval) &&
+			!this.isRightInterval(rightInterval) &&
+			this.isInsideInterval(this.limitInterval)
+		) {
+			this.firstVisiblePage = this.selectedPage - leftInterval;
+			this.lastVisiblePage = this.selectedPage + rightInterval;
+		}
 
-    if (!this.isLeftInterval(leftInterval)
-        && !this.isRightInterval(rightInterval)
-        && this.isInsideInterval(this.limitInterval)) {
-      this.firstVisiblePage =  (this.selectedPage - leftInterval);
-      this.lastVisiblePage = (this.selectedPage + rightInterval);
-    }
+		this.changePagination();
+	}
 
-    this.changePagination();
-  }
+	/**
+	 * Return true if the selected page is in left interval.
+	 *
+	 * @param leftInterval: number
+	 */
+	isLeftInterval(leftInterval: number): boolean {
+		return this.selectedPage <= leftInterval;
+	}
 
-  /**
-   * Return true if the selected page is in left interval.
-   *
-   * @param leftInterval: number
-   */
-  isLeftInterval(leftInterval: number): boolean {
-    return this.selectedPage <= leftInterval;
-  }
+	/**
+	 * Return true if the selected page is in right interval.
+	 *
+	 * @param rightInterval: number
+	 */
+	isRightInterval(rightInterval: number): boolean {
+		return this.selectedPage >= this.totalPages + 1 - rightInterval;
+	}
 
-  /**
-   * Return true if the selected page is in right interval.
-   *
-   * @param rightInterval: number
-   */
-  isRightInterval(rightInterval: number): boolean {
-    return this.selectedPage >= (this.totalPages + 1) - rightInterval;
-  }
+	/**
+	 * Validate if the current page is inside interval.
+	 *
+	 * @param finalinterval: number
+	 */
+	isInsideInterval(finalinterval: number): boolean {
+		if (this.isNext) {
+			return this.selectedPage >= this.lastVisiblePage - finalinterval;
+		} else {
+			return this.selectedPage <= this.firstVisiblePage + finalinterval;
+		}
+	}
 
-  /**
-   * Validate if the current page is inside interval.
-   *
-   * @param finalinterval: number
-   */
-  isInsideInterval(finalinterval: number) {
-    if (this.isNext) {
-        return this.selectedPage >= this.lastVisiblePage - finalinterval;
-    } else {
-        return this.selectedPage <= this.firstVisiblePage + finalinterval;
-    }
-  }
+	/**
+	 * Get last page from any select page.
+	 *
+	 * @param page page number
+	 * @param interval number of interval
+	 */
+	getNextLastPage(page: number, interval: number): number {
+		let rightPage: number = page;
+		const diff =
+			page <= this.limitInterval
+				? page - (this.visiblePages - interval)
+				: 0;
+		while (
+			rightPage < this.totalPages &&
+			rightPage < page + interval - diff
+		) {
+			++rightPage;
+		}
+		return rightPage;
+	}
 
-  /**
-   * Get last page from any select page.
-   *
-   * @param page page number
-   * @param interval number of interval
-   */
-  getNextLastPage(page: number, interval: number) {
-    let rightPage: number = page;
-    const diff = (page <= this.limitInterval ? (page - (this.visiblePages - interval)) : 0 );
-    while (rightPage < this.totalPages
-           && rightPage < (page + interval - diff)) {
-      ++rightPage;
-    }
-    return rightPage;
-  }
-
-  /**
-   * Get first page from last page.
-   *
-   * @param lastPage last page number
-   */
-  getNextFirstPage(lastPage: number) {
-    let leftPage: number = lastPage;
-    while (leftPage > (lastPage - this.visiblePages + 1)
-         && leftPage > FIRST_PAGE) {
-      --leftPage;
-    }
-    return leftPage;
-  }
+	/**
+	 * Get first page from last page.
+	 *
+	 * @param lastPage last page number
+	 */
+	getNextFirstPage(lastPage: number): number {
+		let leftPage: number = lastPage;
+		while (
+			leftPage > lastPage - this.visiblePages + 1 &&
+			leftPage > FIRST_PAGE
+		) {
+			--leftPage;
+		}
+		return leftPage;
+	}
 }
-
--- projects/ugla/src/lib/directives/tooltip.directive.ts
+++ projects/ugla/src/lib/directives/tooltip.directive.ts
@@ -1,67 +1,79 @@
-import { Directive, ElementRef, Input, AfterViewInit, OnChanges, SimpleChanges } from '@angular/core';
+import {
+	Directive,
+	ElementRef,
+	Input,
+	AfterViewInit,
+	SimpleChanges,
+  OnChanges
+} from '@angular/core';
 import Tooltip from 'tooltip.js';
 import { Options, TitleFunction } from 'tooltip.js';
 
 @Directive({
-  selector: '[uglTooltip]',
-  exportAs: 'uglTooltip'
+	selector: '[uglTooltip]',
+	exportAs: 'uglTooltip'
 })
-export class TooltipDirective implements AfterViewInit {
-  _tooltip: any;
+export class TooltipDirective implements AfterViewInit, OnChanges {
 
-  /**
-   * Default tooltip options
-   */
-  _options: Options = {
-    placement: 'top',
-    title: undefined,
-    trigger: 'hover focus',
-    closeOnClickOutside: true,
-  };
+	_tooltip: any;
 
-  /**
-   * Set the tooltip title value
-   */
-  @Input('uglTooltipTitle') title?: string | HTMLElement | TitleFunction;
+	/**
+	 * Default tooltip options
+	 */
+	_options: Options = {
+		placement: 'top',
+		title: undefined,
+		trigger: 'hover focus',
+		closeOnClickOutside: true
+	};
 
-  /**
-   * Set the tooltip options value
-   */
-  @Input('uglTooltip')
-  set options(value: {} | Options) {
-    if (value) {
-      this._options = Object.assign(this._options as object, value);
-    }
-  }
+	/**
+	 * Set the tooltip title value
+	 */
+	@Input('uglTooltipTitle') title?: string | HTMLElement | TitleFunction;
 
-  /**
-   * @ignore
-   */
-  constructor(private elementRef: ElementRef) {}
+	/**
+	 * Set the tooltip options value
+	 */
+	@Input('uglTooltip')
+	set options(value: {} | Options) {
+		if (value) {
+			this._options = Object.assign(this._options as object, value);
+		}
+	}
 
-  /**
-   * Set configurations after view is initializes
-   */
-  ngAfterViewInit() {
-    if (this.title) {
-      this._options.title = this.title;
-    }
-    this._tooltip = new Tooltip((this.elementRef.nativeElement as HTMLElement), this._options);
-  }
+	/**
+	 * @ignore
+	 */
+	constructor(private elementRef: ElementRef) {}
 
-  /**
+	/**
+	 * Set configurations after view is initializes
+	 */
+	ngAfterViewInit(): void {
+		if (this.title) {
+			this._options.title = this.title;
+		}
+		this._tooltip = new Tooltip(
+			this.elementRef.nativeElement as HTMLElement,
+			this._options
+		);
+	}
+
+	/**
    * Set a new tooltip instance
+   * @param value
    */
-  private newTooltipInstance(value: string | HTMLElement | TitleFunction) {
-    if (this._tooltip) {
-      this._tooltip.updateTitleContent(value.toString());
-    }
-  }
+  private newTooltipInstance(value: string | HTMLElement | TitleFunction): void {
+		if (this._tooltip) {
+			this._tooltip.updateTitleContent(value.toString());
+		}
+	}
 
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes.title.currentValue !== changes.title.previousValue) {
-      this._options.title = changes.title.currentValue;
-      this.newTooltipInstance(this._options.title);
-    }
-  }
+	ngOnChanges(changes: SimpleChanges): void {
+		if (changes.title && changes.title.currentValue !== changes.title.previousValue) {
+			this._options.title = changes.title.currentValue;
+			this.newTooltipInstance(this._options.title);
+		}
+	}
 }
